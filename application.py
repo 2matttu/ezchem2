c4d7fa750ed00368ab7356ab232c50424314d369
Matthew Tu <matt.tu@yale.edu>
2018-12-03 03:51:21 +0000

    removed unnecessary code. added go back feature

--- application.py
+++ application.py
@@ -2,25 +2,12 @@
 # deleted ikp3db==1.4
 import os
 
-from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.security import check_password_hash, generate_password_hash
 
 PROPERTIES = ["name", "formula", "mw", "mp", "bp", "density", "amt", "hazards", "precautions"]
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-@app.after_request
-def after_request(response):
-    """Disable caching"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
 
 @app.route('/', methods=["GET", "POST"])
 def index():
